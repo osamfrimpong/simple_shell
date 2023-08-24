8e0615a6ec84a3462ccafefc69d40e723ee7396e
Dr. Schandorf Osam-Frimpong <osamfrimpong@yahoo.com>
2023-08-24 01:51:21 +0000

    Added interactive_odam_functions.c

---
+++ interactive_odam_functions.c
@@ -0,0 +1,39 @@
+#include "shell.h"
+
+/**
+ * initiate_odam_interactive - starts ODAM shell in interactive mode
+ * Return: void
+ */
+
+void initiate_odam_interactive(void)
+{
+	char *command_input = malloc(sizeof(char) * ODAM_TOKEN_BUFFER_SIZE);
+	size_t odam_linecap = 0;
+	int shell_status = 1;
+	ssize_t getline_result;
+	char **parsed_command;
+
+	while (shell_status == 1)
+	{
+		printf("$ ");
+
+		getline_result = getline(&command_input, &odam_linecap, stdin);
+
+		if (getline_result == -1)
+		{
+			shell_status = -1;
+		}
+		else
+		{
+			if (!odam_is_white_space(command_input))
+			{
+				parsed_command = parse_command_input(command_input, ODAM_TOKEN_SEPARATOR);
+				execute_odam_command(parsed_command);
+				free(parsed_command);
+			}
+
+		}
+	}
+
+	free(command_input);
+}
