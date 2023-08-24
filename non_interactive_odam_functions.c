6a06b5d1e6abb9da7b46a7fdcaf198a32f73cb62
Nii-101 <noelamptey101@gmail.com>
2023-08-24 01:42:15 +0000

    Created non-interactive file

---
+++ non_interactive_odam_functions.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+
+/**
+ * initiate_odam_non_interactive - starts ODAM shell in non_interactive mode
+ * Return: void
+ */
+
+void initiate_odam_non_interactive(void)
+{
+	char *command_inputs;
+	char **parsed_command;
+
+	while ((command_inputs = odam_read_lines()) != NULL)
+	{
+		if (!odam_is_white_space(command_inputs))
+		{
+		parsed_command = parse_command_input(command_inputs, ODAM_TOKEN_SEPARATOR);
+		execute_odam_command(parsed_command);
+		free(parsed_command);
+		}
+		free(command_inputs);
+	}
+
+}
